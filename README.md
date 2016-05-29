38924828d8ffd0b5e20204f3bf1a6d93e22312c7
GeebToo <jea.guibert@gmail.com>
2016-05-30 00:01:10 +0200

    Added command arguments to specify the sentence should contain tweets

--- README.md
+++ README.md
@@ -24,8 +24,9 @@ npm install lcdi2c
 ## Run
 
 ```bash
-# to run the crawler to the Twitter API and store the tweets formatted in RabbitMQ
-node get_tweet_worker.js
+# to run the crawler to the Twitter API and store the tweets formatted in RabbitMQ.
+# Specify the sentence you want
+node get_tweet_worker.js "<sentence>" <country code>
 
 # to run the worker which get the tweets in queue of RabbitMQ and displays (in LCD or console)
 node read_tweet_worker.js
--- get_tweet_woker.js
+++ get_tweet_woker.js
@@ -11,17 +11,30 @@ var client = new Twitter({
     access_token_key: config.access_token_key,
     access_token_secret: config.access_token_secret,
 });
-var sentence = "JE VEUX";
+
+if (process.argv.length != 4) {
+    logger.error('Error, missing arguments...');
+    logger.error('To run this program, specify the sentence to use and the country code.');
+    logger.error('Like that : node get_tweet_worker.js "<sentence>" <country code>');
+    logger.error('The order of arguments is important');
+    logger.error('---------- Example ----------');
+    logger.error('node get_tweet_worker.js "Je veux" fr');
+    process.exit(1);
+}
+
+var sentence = process.argv[2];
+var locale = process.argv[3];
 var tweetId = 0;
-var regex = new RegExp(regexStr);
+var regex = new RegExp(createRegex(sentence));
+
 RabbitMQMapper.initPublisher(logger);
 
 setInterval(function() {
     client.get(
         'search/tweets', {
             q: encodeURI('"' + sentence.toLowerCase() + '"') + ' -RT',
-            lang: 'fr',
-            locale: 'fr',
+            lang: locale,
+            locale: locale,
             count: 100,
             result_type: 'recent',
             since_id: tweetId
